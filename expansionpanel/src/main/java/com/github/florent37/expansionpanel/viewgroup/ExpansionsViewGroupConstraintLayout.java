2f40900b25470da418db7cf149c217b3d3aaa5d2
gaara87 <akash87@gmail.com>
2018-09-06 16:49:16 -0700

    androidx version of the library

--- expansionpanel/src/main/java/com/github/florent37/expansionpanel/viewgroup/ExpansionsViewGroupConstraintLayout.java
+++ expansionpanel/src/main/java/com/github/florent37/expansionpanel/viewgroup/ExpansionsViewGroupConstraintLayout.java
@@ -2,13 +2,12 @@ package com.github.florent37.expansionpanel.viewgroup;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.github.florent37.expansionpanel.R;
 
